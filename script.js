c7e8542be8de101eb3d07ee642aeaae55e4a0d38
James Hooper <jameshooperdesigns@gmail.com>
2021-01-28 15:05:03 -0700

    Add populateList, and addItem functions

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const items = [];
 
 function addItem(e) {
   e.preventDefault(); // stop the page from reloading
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  this.reset();
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <label for="">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
 }
 
 addItems.addEventListener('submit', addItem);
